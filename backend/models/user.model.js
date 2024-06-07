92d2e0e57e0c7606d72bf36effe336abe1b4b2ad
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-06-07 11:53:05 +0530

    CA | implemented message ,conversation models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,33 +1,37 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: {
-      values: ['male', 'female'],
-      message: 'Gender must be either male or female',
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: {
+        values: ['male', 'female'],
+        message: 'Gender must be either male or female',
+      },
+    },
+    profilePic: {
+      type: String,
+      default: '',
     },
   },
-  profilePic: {
-    type: String,
-    default: '',
-  },
-});
+  /* created at | updated at | Member since -> <created_at> */
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
