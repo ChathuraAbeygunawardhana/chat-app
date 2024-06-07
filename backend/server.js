d4fea2e5767f1c6436a2073b6e1478075610c5ae
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-06-07 15:23:56 +0530

    CA | implemented message apis

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+import User from './models/user.model.js';
 
 dotenv.config();
 const app = express();
@@ -10,10 +12,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.get('/', (req, res) => {
   /* root route  - http://localhost:5000 */
   res.send('Home route');
+});
+
+app.get('/allUsers', async (req, res) => {
+  const allUsers = await User.find();
+  res.json(allUsers);
 });
 
 app.listen(PORT, () => {
